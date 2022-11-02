a026bc50ee23f4b2fd53adfdb6d89adb7f473f91
Ocknyer <kdu4045@naver.com>
2022-11-02 17:24:31 +0900

    카트 추가 기능 구현

--- js/index.js
+++ js/index.js
@@ -1,43 +1,52 @@
 // 변수 선언
 const btnCola = document.querySelectorAll('.item');
-const cashInHand = document.getElementById('cash-inhand');
-const btnDeposit = document.querySelector('.btn-deposit');
-const restMoney = document.querySelector('.money-rest');
-const inpDeposit = document.querySelector('.inp-deposit');
+const getItemList = document.querySelector('.item-list')
+const cartItems = [];
+// const btnDeposit = document.querySelector('.btn-deposit');
 
+const createUniqueId = () => {
+    const time = `${new Date().getTime()}`.slice(-4);
+    const randomNum = ~~(Math.random() * 10000);
 
+    return `cartItems${time}${randomNum}`
+}
 
-console.log(btnCola)
-btnCola.forEach((value) => {
-    value.addEventListener('click', () => {
-    value.classList.add('push');
+function setCartItem(item, id) {
+    const src = item.querySelector('img').getAttribute('src')
+    const title = item.querySelector('.item-name').textContent;
+    const findItemIdx = cartItems.findIndex((item) => item.id === id)
+    let quantity = 1;
+
+    if (findItemIdx !== -1) {
+        if (cartItems[findItemIdx].quantity >= 5) {
+            item.classList.add('sold-out')
+            return
+        }
+        return cartItems[findItemIdx].quantity++;
+    }
+    cartItems.push({id, src, title, quantity})
+};
+
+const renderCartItem = () => {
+    getItemList.innerHTML = '';
+    
+    cartItems.forEach((item) => {
+        getItemList.innerHTML += `
+        <li>
+            <img src=${item.src}>
+            <p class="list-txt">${item.title}</p>
+            <p class='cola-num'>${item.quantity}</p>
+        </li>
+        `
+    })
+};
+
+btnCola.forEach((item) => {
+    const id = createUniqueId();
+
+    item.addEventListener('click', () => {
+    item.classList.add('push');
+    setCartItem(item, id);
+    renderCartItem();
     })
 });
-
-
-btnDeposit.addEventListener('submit', (e) => {
-    e.preventDefault()
-    restMoney.textContent = inpDeposit.value;
-})
-
-// const colaItem = document.querySelectorAll(".item");
-
-// colaItem.forEach((value) => {
-//   value.addEventListener("click", () => {
-//     const cart = document.querySelector(".item-get-list");
-//     value.classList.add("active");
-//   });
-// });
-
-
-// locationForm.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     const locationOne = job.value
-//     const locationTwo = (10000 / time.value).toFixed();
-//     resultJob.textContent = locationOne;
-//     resultTime.textContent = locationTwo;
-//     loadingTime();
-//     setTimeout(function () {
-//        loadingTimeout();
-//     }, 1300);
-//  });
